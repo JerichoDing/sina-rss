eae58ee3d90cbae3a53ce144db7193d489b0ff67
dingxihu <dingxihu@foxmail.com>
2020-12-17 18:32:45 +0800

    1

--- template/page.js
+++ template/page.js
@@ -3,18 +3,22 @@ fetch('https://cdn.jsdelivr.net/gh/JerichoDing/sina-rss@1.1/dist/rss.json').then
   const items = res.items;
   console.log(111,res);
   const list = document.querySelector('.list');
+  console.log('加载成功');
   list.innerHTML = ''
-  items.forEach(i => {
-    const li = document.createElement('li');
-    const p = document.createElement('p');
-    const timeObj = new Date(i.date_modified);
-    p.innerHTML = `${i.title} （<a href="${i.url}" target="_blank">${timeObj.getHours()}:${timeObj.getMinutes()}</a>）`;
-    li.appendChild(p);
-    list.appendChild(li);
-  });
+  setTimeout(() => {
+    items.forEach(i => {
+      const li = document.createElement('li');
+      const p = document.createElement('p');
+      const timeObj = new Date(i.date_modified);
+      p.innerHTML = `${i.title} （<a href="${i.url}" target="_blank">${timeObj.getHours()}:${timeObj.getMinutes()}</a>）`;
+      li.appendChild(p);
+      list.appendChild(li);
+    });
+  }, 100);
 })
 
 window.onload = function () {
+  
   if (window.console) {
     console.log('%c ᕦ 每日全球财经新闻  copyright from Sina ᕤ','color:#2cccd3;24px -apple-system,Microsoft YaHei,BlinkMacSystemFont,Helvetica Neue,Helvetica,Arial,PingFang SC,Hiragino Sans GB,sans-serif')
   }
